c75bb1b314c312b72c00df27a401ad1adb348430
DevAdalat <101609870+DevAdalat@users.noreply.github.com>
2022-04-16 09:09:33 +0530

    Update nativecall.dart

--- bin/nativecall.dart
+++ bin/nativecall.dart
@@ -3,6 +3,6 @@ import 'dart:ffi';
 import 'generated_bindings.dart';
 
 void main(List<String> arguments) {
-	NativeLibrary library = NativeLibrary(DynamicLibrary.open('lib/libmain.so'));
+	NativeLibrary library = NativeLibrary(DynamicLibrary.open('libmain.so'));
 	print(library.HelloWorld());
 }
